80dce92406cacc598ff1592606173dbbc629ec7e
abdelrahman <abdelrahmanobo12@gmail.com>
2026-03-09 23:45:41 +0200

    init app theme.

---
+++ lib/core/themes/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.blue,
+      ),
+    ),
+  );
+
+}
