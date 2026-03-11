2a64da568dde3da8bc08e2c2d49580f708b01a44
abdelrahman <abdelrahmanobo12@gmail.com>
2026-03-11 14:21:55 +0200

    base response addition

--- lib/config/base_response/base_response.dart
+++ lib/config/base_response/base_response.dart
@@ -5,12 +5,11 @@ class SuccessBaseResponse<T> extends BaseResponse{
   String message;
   String token;
   T user;
-  SuccessBaseResponse(this.message,this.token,this.user);
-
+  SuccessBaseResponse({required this.message,required this.token,required this.user});
 }
 
 class ErrorBaseResponse<T> extends BaseResponse{
   String message;
   int code;
-  ErrorBaseResponse(this.message,this.code);
+  ErrorBaseResponse({required this.message,required this.code});
 }
